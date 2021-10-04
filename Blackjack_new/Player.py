84a9763b911012be3eb42ec9dc8657258e6a86c0
Godiez1 <Godjangg@gmail.com>
2021-10-05 00:48:31 +0700

    No lose my code anymore xD

--- Blackjack_new/Player.py
+++ Blackjack_new/Player.py
@@ -15,14 +15,11 @@ class Player:
     def __repr__(self):
         return self.name
 
-    def draw(self, deck):
-        self.hand.append(deck.pop())
-
     @property
     def value(self):
         return self.calculate_val()
 
-    @value.setter
+    @value.getter
     def calculate_val(self):
         s = 0
         for card in self.hand:
@@ -39,6 +36,48 @@ class Player:
     def set_player_money(cls, money):
         cls.initial_money = money
 
+    def draw(self, deck, more=False):
+        tmp = deck.pop()
+        self.hand.append(tmp)
+        if more:
+            print(f'{self} has Draw {tmp}')
+
     def show_hand(self):
         print(f'{self.name} had {self.hand}')
         print(f'value = {self.value}')
+
+    def show_unblined_card(self):
+        print(f'{self}:')
+        print(f'have: {self.hand[0]}')
+
+
+class Black_Jack_player(Player):
+    
+    def __init__(self, name):
+        super().__init__(name)
+        self.money = Black_Jack_player.initial_money
+
+    @property
+    def value(self):
+        return self.calculate_val()
+
+    @value.getter
+    def calculate_val(self):
+        s = ace_count = 0
+        for card in self.hand:
+            val = card.split()[0]
+            if val == 'Ace':
+                ace_count += 1
+            elif val in ('King', 'Queen', 'Jack', 'King'):
+                s += 10
+            else:
+                s += int(val)
+
+        while ace_count:
+            if s + 11 <= 23:
+                s += 11
+            else:
+                s += 1
+            ace_count -= 1
+
+        return s
